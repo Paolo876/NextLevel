d05babf51d655b7ab0d3981d06e6c559551b29b4
paoloBugarin <paolopaolobugarin@gmail.com>
2024-09-08 21:16:53 -0700

    updated meal page slug

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,26 +1,31 @@
 import classes from "./page.module.css"
 import Image from "next/image";
+import { getMeal } from "@/lib/meals";
 
-function MealDetails() {
+
+function MealDetails({ params }) {   //params can be passed as props on slug pages, no need to import useParams
+    const meal = getMeal(params.slug);
+
+    const instructions = meal.instructions.replace(/\n/g, '<br/>')  //update html format
     return (
         <>
             <header className={classes.header}>
                 <div className={classes.image}>
-                    <Image fill/>
+                    <Image fill src={meal.image}/>
                 </div>
                 <div className={classes.headerText}>
-                    <h1>TITLE</h1>
+                    <h1>{meal.title}</h1>
                     <p className={classes.creator}>
-                        by <a href={`mailto:${'email'}`}>NAME</a>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
                     </p>
-                    <p className={classes.summary}>SUMMARY</p>
+                    <p className={classes.summary}>{meal.summary}</p>
                 </div>
             </header>
             <main>
                 <p 
                     className={classes.instructions} 
-                    dangerouslySetInnerHTML={{
-                        __html: "...",
+                    dangerouslySetInnerHTML={{      //this is used because the data stored is in html format
+                        __html: instructions,
                     }}
                 ></p>
             </main>
